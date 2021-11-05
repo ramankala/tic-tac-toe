db526319278706f2b7628edbb5f09a5cfed34974
ramankala <rs.kala@hotmail.com>
2021-11-05 03:05:47 -0400

    Player object completed
    Player object was created with factory function.

--- app.js
+++ app.js
@@ -11,4 +11,10 @@ const gameBoard = (() => {
     return {createBoard};
 })();
 
+const Player = (name, marks) => {
+    const sayName = () => console.log(`my name is ${name} and my mark is ${marks}.`)
+
+    return {name, marks, sayName};
+}
+
 gameBoard.createBoard();
