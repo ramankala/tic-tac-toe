c567f4fdaae9041229349283aa3605b83d70a2cf
Raman <rs.kala@hotmail.com>
2021-11-24 02:22:59 -0500

    completed modules and Player factory

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
-const gameBoard = (() => {
-    function createBoard(){
 
-        const board = [
-            (" ", " ", " "),
-            (" ", " ", " "),
-            (" ", " ", " ")
-        ]
-        console.log(board);
-    }
-    return {createBoard};
+const Player = (name, mark) => {
+    const sayName = () => console.log(`my name is ${name} and my mark is ${mark}.`)
+
+    return {name, mark, sayName};
+};
+
+const gameBoard = (() => {
+
+    const playerArr = [
+        Player("raman", "x")
+    ];
+
+
+    const board = [
+        (" ", " ", " "),
+        (" ", " ", " "),
+        (" ", " ", " ")
+    ]
+
+
+    const displayController =(() => {
+        // DOM Manipulation stuff goes here, refer to ODIN lesson if you forget
+        // Appending probably goes in the render function, just worry about DOM creation first
+        // createBoard is returned making it available to use, can use it here for DOM
+        // Run a for loop on the board and create a div for each entry
+        // display each div and put styles
+        // can toggle player active status
+        // create attribute to toggle player's status, either here(most likely this) or in gameBoard
+
+        const container = document.querySelector('#container');
+
+        board.forEach(function(item, index, array){
+            squareDiv = document.createElement("div");
+            squareDiv.setAttribute("id", "squareDiv");
+        });
+
+
+    })();
+
+    // playerArr.forEach(function(item, index, array){
+    //     console.log(item);
+    // })
+
+
+return {board, playerArr};
 })();
 
-const Player = (name, marks) => {
-    const sayName = () => console.log(`my name is ${name} and my mark is ${marks}.`)
 
-    return {name, marks, sayName};
-}
 
-gameBoard.createBoard();
+
